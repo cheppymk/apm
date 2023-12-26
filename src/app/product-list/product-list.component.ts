9665599bd2a3d13d1ce9fcb55d90081afcfb163f
cheppymk <toso.micevski@gmail.com>
2023-12-26 15:56:28 +0100

    product-list-component

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-product-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+
+}
